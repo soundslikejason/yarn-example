dd3c0bea2ddcf1a79f1a8eb71de5822f4c8fdb8a
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-06 07:49:55 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+let app = express();
+
+app.get('/', (req, res) => {
+  res.send('This is my yarn project');
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
